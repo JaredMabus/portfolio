48d87f1e1a5d12d8f658a8df7b9f6537c166b2f7
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-03-14 23:03:36 -0500

    flask catch all route change

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder="../portfolio_react/build/static", template_folder="../portfolio_react/build")
 
 
-# Routes non-api traffic to react app
-@app.errorhandler(404)
-def handle_404(e):
+# # Routes non-api traffic to react app
+# @app.errorhandler(404)
+# def handle_404(e):
+#     return render_template("index.html")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
     return render_template("index.html")
 
 # API routes
