2dc6aa13c115053f0af35e8dc1c546ab023ce6f6
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-04-12 13:48:47 -0500

    ga

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import {
   BrowserRouter as Router,
@@ -8,12 +8,18 @@ import {
 } from "react-router-dom";
 import { GlobalStyle, light, dark } from "./styles/components";
 import Home from "./components/Home";
+import ReactGa from "react-ga";
 
 function App() {
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
+
+  useEffect(() => {
+    ReactGa.initialize("UA-225621926-1");
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  });
 
   return (
     <>
