a7b2863f85fc0378a25061ce13cf40de7b045c61
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-12-05 13:06:47 -0600

    added express server

---
+++ server/server.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.port || 3001;
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("*", (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  } catch (err) {
+    res.status(500);
+    throw new Error("could not send index.html");
+  }
+});
+
+app.use(async (err: Error, req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.json({ err: true, msg: err.message });
+  } catch (err) {
+    res.status(500);
+    res.json({ err: true, msg: "Interal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening at -- http://localhost:${port} -- 🚀`);
+});
